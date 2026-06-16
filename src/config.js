197f79e0fcdd5702e19813c5cc68910b3f461583
deep <dipvadukiya06@gmail.com>
2026-06-16 22:14:32 +0200

    removed google auth tocken

--- src/config.js
+++ src/config.js
@@ -7,5 +7,4 @@ export const LOCATION = "us-central1";
 export const ENDPOINT_ID = "1329292515550953472";
 
 // get the access token from the Google Cloud Console and use it for testing ...
-export const ACCESS_TOKEN =
-  "ya29.a0AT3oNZ-iE55-4Hwc5-adEe7Hz6m_1aDoWdGc0nwti4dRHuLLbEeykvNl_88kOLNo09WHd902jLT41f5qf_hi7_e1HtMuatBLh9M1UXMacTJK3A652Kgm555z2VbCL8qIKaSdp6nkqr5TaS3Rv9RBhZl82t8e9qbcs81bSFJ6jTZf1kE5Q-9z-nlt6d0LLpPRZDg4LmhLG-kAaCgYKASESARMSFQHGX2MiUf6epWie_PawsviuRwB6AA0211";
+export const ACCESS_TOKEN = "";
