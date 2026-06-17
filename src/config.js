84f6fee22f6eb4f57f8a47628998ddeb9aa4a6dc
deep <dipvadukiya06@gmail.com>
2026-06-17 17:30:29 +0200

    updated project id

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ export const PROJECT_ID = "automl-vs-custom-training";
 
 export const LOCATION = "us-central1";
 
-export const ENDPOINT_ID = "1329292515550953472";
+export const ENDPOINT_ID = "8654960259422486528";
 
 // get the access token from the Google Cloud Console and use it for testing ...
 export const ACCESS_TOKEN = "";
