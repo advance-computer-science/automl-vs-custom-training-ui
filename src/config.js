f6162110cfc341f2e1301353b2e5e201403c0324
deep <dipvadukiya06@gmail.com>
2026-06-17 19:48:41 +0200

    added config for custom training

--- src/config.js
+++ src/config.js
@@ -8,3 +8,6 @@ export const ENDPOINT_ID = "8654960259422486528";
 
 // get the access token from the Google Cloud Console and use it for testing ...
 export const ACCESS_TOKEN = "";
+
+export const CUSTOM_TRAINING_ENDPOINT =
+  "https://w2gz9tm2-8000.euw.devtunnels.ms";
