166dc58abaf2ae20f4d13fa4e9e66ff0dca03a3f
deep <dipvadukiya06@gmail.com>
2026-06-18 17:08:26 +0200

    removed google key from the config and added into the local storage

--- src/config.js
+++ src/config.js
@@ -1,12 +1,11 @@
 //
 
+export const STORAGE_KEY = "google_api_key";
+
 export const PROJECT_ID = "automl-vs-custom-training";
 
 export const LOCATION = "us-central1";
 
 export const ENDPOINT_ID = "5258964665408421888";
-
-// get the access token from the Google Cloud Console and use it for testing ...
-export const ACCESS_TOKEN = "";
 
 export const CUSTOM_TRAINING_ENDPOINT = "http://127.0.0.1:8000";
