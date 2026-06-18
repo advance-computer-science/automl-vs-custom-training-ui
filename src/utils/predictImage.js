73a06f7daec5fb50af678cbcb7631b3e1e1105c3
deep <dipvadukiya06@gmail.com>
2026-06-18 17:09:06 +0200

    used the google api kley from the local storage and updated new google key popup while 401 error

--- src/utils/predictImage.js
+++ src/utils/predictImage.js
@@ -1,19 +1,30 @@
 //
 
 import { convertToBase64 } from "./base64";
-import { LOCATION, PROJECT_ID, ENDPOINT_ID, ACCESS_TOKEN } from "../config";
+import { LOCATION, PROJECT_ID, ENDPOINT_ID, STORAGE_KEY } from "../config";
 
 // ----------------------------------------------
 
 export const predictImage = async (file) => {
   const imageBytes = await convertToBase64(file);
 
+  let storedKey = localStorage.getItem(STORAGE_KEY);
+
+  if (!storedKey.startsWith("ya29.")) {
+    const newKey = window.prompt("Please enter your Google API Key");
+
+    if (newKey?.trim()) {
+      localStorage.setItem(STORAGE_KEY, newKey.trim());
+      storedKey = newKey.trim();
+    }
+  }
+
   const response = await fetch(
     `https://${LOCATION}-aiplatform.googleapis.com/v1/projects/${PROJECT_ID}/locations/${LOCATION}/endpoints/${ENDPOINT_ID}:predict`,
     {
       method: "POST",
       headers: {
-        Authorization: `Bearer ${ACCESS_TOKEN}`,
+        Authorization: `Bearer ${storedKey}`,
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
@@ -29,7 +40,11 @@ export const predictImage = async (file) => {
   const result = await response.json();
 
   if (result?.error?.code === 401) {
-    window.alert("Unauthorized access. Please check your access token.");
+    const newKey = window.prompt(
+      "Unauthorized access. Please enter the new Google API Key",
+    );
+    localStorage.setItem(STORAGE_KEY, newKey.trim());
+    window.location.reload();
   } else if (result?.error?.code === 404) {
     window.alert(
       "No end point found. Please check your endpoint ID and project configuration.",
